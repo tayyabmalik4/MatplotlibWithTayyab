3a806837cdd3dc7b7c47af476121d30bd193c500
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-07-30 16:17:18 +0500

    1.3

--- 04_Histogram_Matp;otlib.py
+++ 04_Histogram_Matp;otlib.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 import random
+from matplotlib import style
 
 np.random.seed(250)
 ml_student_age=np.random.randint(20,50, (100))
@@ -15,6 +16,13 @@ py_student_age=np.random.randint(10,40, (100))
 
 # /////if we want to change the graph category wise than we use bin function
 bins=[10,15,20,25,30,35,40,45,50]
+# /////if we want to large or small graph than we use figure function in matplotlib
+plt.figure(figsize=(16,9))
+
+# ////if we want to change the background color than we use style module and function
+style.use('ggplot')
+plt.grid(color='k')
+
 # plt.hist(ml_student_age,bins)
 
 # /////When we want to seperate the lines of graphs than we use rwidth function
@@ -30,8 +38,10 @@ bins=[10,15,20,25,30,35,40,45,50]
 # plt.hist(ml_student_age,bins,rwidth=0.8, orientation='vertical', color='red')
 
 # ////When we WANT to creat the name of the table than we use label function and also use the lagend function spacificly
-plt.hist(ml_student_age,bins,rwidth=0.8, orientation='vertical', color='red',label='ML Students',align='right',stacked=True)
-plt.hist(py_student_age,bins,rwidth=0.5, orientation='vertical', color='black',label='py Student Age')
+# plt.hist(ml_student_age,bins,rwidth=0.8, orientation='vertical', color='red',label='ML Students',align='right',stacked=True)
+# plt.hist(py_student_age,bins,rwidth=0.5, orientation='horizontal', color='black',label='py Student Age')
+# /////when we want to print out together and difference properly than we use list in the program
+plt.hist([ml_student_age,py_student_age], bins, rwidth=0.8,orientation='vertical', color=['r','y'],label=['ML Students', 'PY students'])
 plt.legend(loc=1)
 # ////crating title of the graph
 plt.title("Machine Learning Student age in histogram")
---
+++ 05_bar_chart_matplotlib.py
@@ -0,0 +1,32 @@
+# *********************************Bar chart using matplotlib**********************
+# ///////this is the use to creat the grasph using some arguments
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import style
+
+# //////starting the values
+classes=['python','Machine Learning','Data Science','Robotics','Deep learning']
+np.random.seed(100)
+class1=np.random.randint(10,30,(5))
+class2=np.random.randint(10,30,(5))
+class3=np.random.randint(15,30,(5))
+# plt.bar(classes,class1)
+
+# /////when we want to plot the bar chart in horizontal than we use barh function
+# plt.barh(classes,class1)
+
+# //////these are the functions of the bar chart
+# /////width is use to define the width along lines
+# /////align is use to define the place
+# ////color ios use to define the color
+# ////edgecolor is use to define the edge color
+# /////linewidth is use to define the wedth in the line
+# /////alpha is use to minmize the opacity
+# /////linestyle is use to style of the lines
+# /////label is use to creat name of the chart 
+# ////visible is use to define that the chart is visible or nor if visible False chart is not visible-----default(visible=True) 
+plt.bar(classes,class1, width=0.5, align='edge', color='yellow',edgecolor='m',linewidth=5,alpha=0.5,linestyle='--',label='Class 1 Students',visible=True)
+plt.legend(loc=2)
+
+plt.show()
