cc9242fae5f9a3f7c070b7ba239e373cee5b0933
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-07-31 08:09:00 +0500

    1.3

---
+++ 12_image_show_colorbar_matplotlib.py
@@ -0,0 +1,52 @@
+# (12)*********************Show Image And colorbar in matplotlib********
+# //////if we want to read the image data than we use image using matplotlib in python
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+# /////we use imread function to showing the image
+img=mpimg.imread('11.2_Figure_2.png')
+img1=mpimg.imread('11.1_Figure_1.png')
+# print(img)
+
+# ///checking type of the image
+# print(type(img))
+
+# /////chacking the how many rows and columns or attributes
+# print(img.shape)
+
+# /////checking the dimentional
+# print(img.ndim)
+
+# ////if we want to maximize or minnize the figure size thanwe use figure function in matplotlib
+# plt.figure(figsize=(13,13))
+
+# ////if we want don't show the axis than we use axis('off)
+# plt.axis('off')
+
+# /////we use these parameters in the cmap function
+""""'Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 
+'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 
+'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r'"""
+# ////if we use cmap properly than we must use the 2d array instance of 3d arrray
+# arr2d=img[1:,:,1]
+# arr2d=img1[:,:,2]
+# /////to showing the image use imshow function
+# plt.imshow(img,cmap='spring')
+# plt.imshow(arr2d,cmap='hot')
+# /////if we want to show the side color bar than we use colorbar function and 
+# ////this detect automatic colors which is exists in the figure
+# plt.colorbar()
+
+# plt.show()
+
+
+
+
+# ****************this is the uploading pic through path and showing and also making pangas
+plt.figure(figsize=(13,13))
+img2=mpimg.imread('F:\\tayyab programming\\machine learning\\Matplotlib\\matplotlibWithTayyab\\images\\tayyab chaye.jpg')
+plt.axis('off')
+plt.imshow(img2)
+plt.colorbar()
+plt.show()
