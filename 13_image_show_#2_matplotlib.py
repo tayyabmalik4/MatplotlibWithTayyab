0d98e26c2dbc43119d0cb192be589e1a29cab9f8
Tayyab Malik <mtayyabmalik99@gmail.com>
2021-08-01 00:15:02 +0500

    1.3

--- 13_image_show_#2_matplotlib.py
+++ 13_image_show_#2_matplotlib.py
@@ -3,3 +3,34 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+img3=mpimg.imread('F:\\tayyab programming\\machine learning\\Matplotlib\\matplotlibWithTayyab\\images\\tayyab chaye.jpg')
+plt.figure(figsize=(13,13))
+plt.axis('off')
+single_channal_img=img3[:,:,1]
+
+# plt.imshow(img3,cmap='hot')
+plt.imshow(single_channal_img,cmap='nipy_spectral')
+
+plt.colorbar()
+
+# /////we use these parameters in the cmap function
+cmap="""'Accent', 'Accent_r', 'Blues', 'Blues_r', 'BrBG', 'BrBG_r', 'BuGn', 'BuGn_r', 'BuPu', 'BuPu_r', 'CMRmap', 'CMRmap_r', 'Dark2', 'Dark2_r', 'GnBu', 'GnBu_r', 'Greens', 'Greens_r', 'Greys', 'Greys_r', 'OrRd', 'OrRd_r', 
+'Oranges', 'Oranges_r', 'PRGn', 'PRGn_r', 'Paired', 'Paired_r', 'Pastel1', 'Pastel1_r', 'Pastel2', 'Pastel2_r', 'PiYG', 'PiYG_r', 'PuBu', 'PuBuGn', 'PuBuGn_r', 'PuBu_r', 'PuOr', 'PuOr_r', 'PuRd', 'PuRd_r', 'Purples', 'Purples_r', 'RdBu', 'RdBu_r', 'RdGy', 'RdGy_r', 'RdPu', 'RdPu_r', 'RdYlBu', 'RdYlBu_r', 'RdYlGn', 'RdYlGn_r', 'Reds', 'Reds_r', 'Set1', 'Set1_r', 'Set2', 'Set2_r', 'Set3', 'Set3_r', 'Spectral', 'Spectral_r', 'Wistia', 'Wistia_r', 'YlGn', 'YlGnBu', 'YlGnBu_r', 'YlGn_r', 'YlOrBr', 'YlOrBr_r', 'YlOrRd', 'YlOrRd_r', 'afmhot', 'afmhot_r', 'autumn', 'autumn_r', 'binary', 'binary_r', 'bone', 'bone_r', 'brg', 'brg_r', 'bwr', 'bwr_r', 'cividis', 'cividis_r', 'cool', 'cool_r', 'coolwarm', 'coolwarm_r', 'copper', 'copper_r', 'cubehelix', 'cubehelix_r', 'flag', 'flag_r', 'gist_earth', 'gist_earth_r', 'gist_gray', 'gist_gray_r', 'gist_heat', 
+'gist_heat_r', 'gist_ncar', 'gist_ncar_r', 'gist_rainbow', 'gist_rainbow_r', 'gist_stern', 'gist_stern_r', 'gist_yarg', 'gist_yarg_r', 'gnuplot', 'gnuplot2', 'gnuplot2_r', 'gnuplot_r', 'gray', 'gray_r', 'hot', 'hot_r', 'hsv', 'hsv_r', 'inferno', 'inferno_r', 'jet', 'jet_r', 'magma', 'magma_r', 'nipy_spectral', 'nipy_spectral_r', 'ocean', 'ocean_r', 'pink', 'pink_r', 'plasma', 'plasma_r', 'prism', 'prism_r', 'rainbow', 'rainbow_r', 'seismic', 'seismic_r', 'spring', 'spring_r', 'summer', 'summer_r', 'tab10', 'tab10_r', 'tab20', 'tab20_r', 'tab20b', 'tab20b_r', 'tab20c', 'tab20c_r', 'terrain', 'terrain_r', 'turbo', 'turbo_r', 'twilight', 'twilight_r', 'twilight_shifted', 'twilight_shifted_r', 'viridis', 'viridis_r', 'winter', 'winter_r'"""
+split_elements=cmap.split(' , ')
+# print(split_elements)
+# split_elements=[]
+# for i in range(len(split_elements)):
+#     cmap_str=split_elements[i]
+#     plt.figure(figsize=(13,13))
+#     plt.axis('off')
+#     single_channal_img=img3[:,:,1]
+#     plt.imshow(single_channal_img,cmap=cmap_str)
+    # plt.colorbar()
+    # save_image_list.append('F:\\tayyab programming\\machine learning\\Matplotlib\\matplotlibWithTayyab\\images\\converting images\\'+'new_'+split_elements[i]+'.jpg')
+    # print(split_elements[i])
+
+    # plt.savefig('F:\\tayyab programming\\machine learning\\Matplotlib\\matplotlibWithTayyab\\images\\converting images\\new.png')
+    # plt.show()
+plt.savefig('F:\\tayyab programming\\machine learning\\Matplotlib\\matplotlibWithTayyab\\images\\converting images\\new.png')
+plt.show()
